61a8fa259b7adb86b77c355005f78e8a1cebfdc4
Park Juhyung <majecty@gmail.com>
2023-03-28 00:28:15 +0900

    Finish 3.4

--- src/03_Logic/04_Conjunction_and_Bi-implication.lean
+++ src/03_Logic/04_Conjunction_and_Bi-implication.lean
@@ -7,7 +7,7 @@ begin
   { assumption },
   intro h,
   apply h₁,
-  rw h
+  rw h,
 end
 
 example {x y : ℝ} (h₀ : x ≤ y) (h₁ : ¬ y ≤ x) : x ≤ y ∧ x ≠ y :=
@@ -48,8 +48,15 @@ example {x y : ℝ} (h : x ≤ y ∧ x ≠ y) : ¬ y ≤ x :=
 λ h', h.right (le_antisymm h.left h')
 
 example {m n : ℕ} (h : m ∣ n ∧ m ≠ n) :
-  m ∣ n ∧ ¬ n ∣ m :=
-sorry
+  m ∣ n ∧ ¬ n ∣ m := begin
+  split,
+  { exact h.left },
+  { show ¬ n ∣ m,
+    contrapose! h,
+    intro nm,
+    exact nat.dvd_antisymm nm h,
+  },
+end
 
 example : ∃ x : ℝ, 2 < x ∧ x < 4 :=
 ⟨5/2, by norm_num, by norm_num⟩
@@ -96,17 +103,69 @@ example {x y : ℝ} (h : x ≤ y) : ¬ y ≤ x ↔ x ≠ y :=
 ⟨λ h₀ h₁, h₀ (by rw h₁), λ h₀ h₁, h₀ (le_antisymm h h₁)⟩
 
 example {x y : ℝ} : x ≤ y ∧ ¬ y ≤ x ↔ x ≤ y ∧ x ≠ y :=
-sorry
+begin
+split,
+{ show x ≤ y ∧ ¬y ≤ x → x ≤ y ∧ x ≠ y,
+  rintros ⟨ xlty, nyltx⟩,
+  split,
+  { show x ≤ y,
+    exact xlty },
+  { show x ≠ y,
+    contrapose! nyltx,
+    linarith,
+  }
+},
+{ show x ≤ y ∧ x ≠ y → x ≤ y ∧ ¬y ≤ x,
+  rintros ⟨ xlty, nyltx ⟩,
+  split,
+  { show x ≤ y,
+    exact xlty },
+  { show ¬y ≤ x,
+    contrapose! nyltx,
+    linarith,
+  }
+},
+end
 
 theorem aux {x y : ℝ} (h : x^2 + y^2 = 0) : x = 0 :=
 begin
   have h' : x^2 = 0,
-  { sorry },
+  { 
+    have y2gezero : y^2 ≥ 0,
+    { exact sq_nonneg y,},
+    have x2gezero : x^2 ≥ 0,
+    { exact sq_nonneg x },
+    linarith,
+   },
   exact pow_eq_zero h'
 end
 
-example (x y : ℝ) : x^2 + y^2 = 0 ↔ x = 0 ∧ y = 0 :=
-sorry
+example (x y : ℝ) : x^2 + y^2 = 0 ↔ x = 0 ∧ y = 0 := begin
+  split,
+  { show x ^ 2 + y ^ 2 = 0 → x = 0 ∧ y = 0,
+    intros x2py2eq0,
+    have xsqgte0 : x^2 ≥ 0,
+    { exact sq_nonneg x },
+    have ysqgte0 : y^2 ≥ 0,
+    { exact sq_nonneg y },
+    have xsqzero : x^2 = 0,
+    { linarith },
+    have ysqzero : y^2 = 0, by linarith,
+    have xzero : x = 0, by exact pow_eq_zero xsqzero,
+    have yzero : y = 0, by exact pow_eq_zero ysqzero,
+    exact ⟨xzero, yzero⟩,
+  },
+  { show x = 0 ∧ y = 0 → x ^ 2 + y ^ 2 = 0,
+    rintros ⟨xzero, yzero⟩,
+    have xsqzero : x^2 = 0, {
+      exact sq_eq_zero_iff.mpr xzero,
+    },
+    have ysqzero : y^2 = 0, {
+      exact sq_eq_zero_iff.mpr yzero,
+    },
+    linarith,
+  },
+end
 
 section
 
@@ -129,8 +188,13 @@ theorem not_monotone_iff {f : ℝ → ℝ}:
   ¬ monotone f ↔ ∃ x y, x ≤ y ∧ f x > f y :=
 by { rw monotone, push_neg }
 
-example : ¬ monotone (λ x : ℝ, -x) :=
-sorry
+example : ¬ monotone (λ x : ℝ, -x) := begin
+  rintros monomx,
+  rw monotone at monomx,
+  contrapose! monomx,
+  use [0, 1],
+  norm_num,
+end
 
 section
 variables {α : Type*} [partial_order α]
@@ -139,7 +203,28 @@ variables a b : α
 example : a < b ↔ a ≤ b ∧ a ≠ b :=
 begin
   rw lt_iff_le_not_le,
-  sorry
+  split,
+  { show a ≤ b ∧ ¬b ≤ a → a ≤ b ∧ a ≠ b,
+    rintros ⟨ aleb, nblea ⟩ ,
+    split,
+    { show a ≤ b, exact aleb, },
+    { show a ≠ b,
+      contrapose! nblea,
+      exact eq.ge nblea,
+    }
+  },
+  { show a ≤ b ∧ a ≠ b → a ≤ b ∧ ¬b ≤ a,
+    rintros ⟨ aleb, aneqb ⟩, 
+    split,
+    { show a ≤ b, exact aleb,  },
+    { show ¬b ≤ a,
+      contrapose! aneqb,
+      -- antisym을 써도 되나? preorder, partial order 헷갈리네
+      -- partial order: refl, trans, antisymm
+      -- preorder : reflexive, transitive
+      exact ge_antisymm aneqb aleb,
+    },
+  },
 end
 
 end
@@ -148,16 +233,49 @@ section
 variables {α : Type*} [preorder α]
 variables a b c : α
 
+-- only use le_refl, le_antisymm
+-- . You do not need anything more than le_refl and le_trans.
 example : ¬ a < a :=
 begin
   rw lt_iff_le_not_le,
-  sorry
+  by_contradiction h,
+  have alea : a ≤ a, exact h.left,
+  have nalea : ¬a ≤ a, exact h.right,
+  exact nalea alea,
 end
 
+example (p q : Prop) (h : p → q ) : ¬q → ¬p  := begin
+exact mt h,
+end
+
+example (p q r : Prop) (h : p → q → r) : (¬(q → r)) → ¬ p     := begin
+exact mt h,
+end
+
+example (p q r : Prop) (h : p → q → r) : (¬r → ¬q) → ¬ p     := begin
+sorry,
+-- library_search,
+end
+
+-- class preorder (α : Type u) extends has_le α, has_lt α :=
+-- (le_refl : ∀ a : α, a ≤ a)
+-- (le_trans : ∀ a b c : α, a ≤ b → b ≤ c → a ≤ c)
+-- (lt := λ a b, a ≤ b ∧ ¬ b ≤ a)
+-- (lt_iff_le_not_le : ∀ a b : α, a < b ↔ (a ≤ b ∧ ¬ b ≤ a) . order_laws_tac)
 example : a < b → b < c → a < c :=
 begin
   simp only [lt_iff_le_not_le],
-  sorry
+  rintros abba bccb,
+  have alec : a ≤ c, apply le_trans abba.left bccb.left,
+  split,
+  { show  a ≤ c, exact alec, },
+  { show ¬c ≤ a,
+    by_contradiction,
+    have cleb : c ≤ b, begin
+     apply le_trans h abba.left,
+    end,
+    exact bccb.right cleb,
+  },
 end
 
 end

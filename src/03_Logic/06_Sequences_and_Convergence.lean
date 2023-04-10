efba2679b39bca067325bab09802e80d1e0813a0
Park Juhyung <majecty@gmail.com>
2023-04-11 00:43:29 +0900

    wip 3.6

--- src/03_Logic/06_Sequences_and_Convergence.lean
+++ src/03_Logic/06_Sequences_and_Convergence.lean
@@ -25,6 +25,7 @@ begin
   apply εpos
 end
 
+-- 이것보다 잘 할 수 있을 거 같은데
 theorem converges_to_add {s t : ℕ → ℝ} {a b : ℝ}
   (cs : converges_to s a) (ct : converges_to t b):
 converges_to (λ n, s n + t n) (a + b) :=
@@ -35,9 +36,23 @@ begin
   cases cs (ε / 2) ε2pos with Ns hs,
   cases ct (ε / 2) ε2pos with Nt ht,
   use max Ns Nt,
-  sorry
+  intros bigger_n max_N_le_n,
+  -- use n for `hs: ∀ (n : ℕ), n ≥ Ns → |s n - a| < ε / 2`,
+  have hs' : abs (s bigger_n - a) < ε / 2,
+  { apply hs, exact le_of_max_le_left max_N_le_n, },
+  have ht' : abs (t bigger_n - b) < ε / 2,
+  { apply ht, exact le_of_max_le_right max_N_le_n, },
+  have eta_eq_two_eta_div_two : ε = (ε / 2) + (ε / 2), by ring,
+  have hs'_plus_ht' : abs (s bigger_n - a) + abs (t bigger_n - b) < ε,
+  { rw eta_eq_two_eta_div_two, linarith },
+  have triangle_ineq : abs (s bigger_n + t bigger_n - (a + b)) ≤ abs (s bigger_n - a) + abs (t bigger_n - b),
+  { have k: s bigger_n + t bigger_n - (a + b) = (s bigger_n - a) + (t bigger_n - b), { ring },
+    rw k,
+    exact abs_add _ _ },
+  linarith,
 end
 
+-- def converges_to (s : ℕ → ℝ) (a : ℝ) := ∀ ε > 0, ∃ N, ∀ n ≥ N, abs (s n - a) < ε
 theorem converges_to_mul_const {s : ℕ → ℝ} {a : ℝ}
     (c : ℝ) (cs : converges_to s a) :
   converges_to (λ n, c * s n) (c * a) :=
@@ -48,16 +63,52 @@ begin
     rw [h, zero_mul] },
   have acpos : 0 < abs c,
     from abs_pos.mpr h,
-  sorry
+
+  intros ε ε_pos, dsimp,
+  cases cs (ε / abs c) (div_pos ε_pos acpos) with N h,
+  use N,
+  intros n n_gte_N,
+  have h2: |s n - a| < ε / |c|, {
+    apply h, linarith,
+  },
+  have h3: |c| * |s n - a| < ε, {
+    rw mul_comm,
+    apply (lt_div_iff acpos).mp,
+    linarith,
+  },
+  have h4: |c| * |s n - a| = |c * s n - c * a|, {
+    rw ← abs_mul c _,
+    ring_nf,
+  },
+  linarith,
 end
 
+-- def converges_to (s : ℕ → ℝ) (a : ℝ) := ∀ ε > 0, ∃ N, ∀ n ≥ N, abs (s n - a) < ε
 theorem exists_abs_le_of_converges_to {s : ℕ → ℝ} {a : ℝ}
     (cs : converges_to s a) :
   ∃ N b, ∀ n, N ≤ n → abs (s n) < b :=
 begin
   cases cs 1 zero_lt_one with N h,
   use [N, abs a + 1],
-  sorry
+  intros n n_ge_N,
+  show |s n| < |a| + 1,
+  have h1: |s n - a| < 1, begin
+    apply h, linarith,
+  end,
+  have h2: |s n - a| + |a| < |a| + 1, begin
+    linarith,
+  end,
+  have h3: |s n| ≤ |s n - a| + |a|, {
+    have h3_1: |s n - a + a| ≤ |s n - a| + |a|, {
+      apply abs_add,
+    },
+    have h3_2: |s n - a + a| = |s n|, {
+      ring_nf,
+    },
+    rw ← h3_2,
+    exact h3_1,
+  },
+  linarith,
 end
 
 lemma aux {s t : ℕ → ℝ} {a : ℝ}

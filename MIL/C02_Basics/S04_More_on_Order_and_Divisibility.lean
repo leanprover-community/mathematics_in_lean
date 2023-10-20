ec2458436ce8cad06be8f1be677827fc575f24ff
Jeremy Toh <7417963+deepimpactmir@users.noreply.github.com>
2023-10-20 15:28:45 +0800

    Completed S04

--- MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
+++ MIL/C02_Basics/S04_More_on_Order_and_Divisibility.lean
@@ -62,7 +62,7 @@ example : min (min a b) c = min a (min b c) := by
   apply le_min
   · apply le_min
     · apply min_le_left
-    apply le_trans 
+    apply le_trans
     apply min_le_right
     apply min_le_left
   apply le_trans
@@ -70,13 +70,29 @@ example : min (min a b) c = min a (min b c) := by
   apply min_le_right
 
 theorem aux : min a b + c ≤ min (a + c) (b + c) := by
-  sorry
+  apply le_min
+  · apply add_le_add_right
+    apply min_le_left
+  apply add_le_add_right
+  apply min_le_right
+
 example : min a b + c = min (a + c) (b + c) := by
-  sorry
+  apply le_antisymm
+  · apply aux
+  have h : min (a + c) (b + c) = min (a + c) (b + c) - c + c := by rw [sub_add_cancel]
+  rw [h]
+  apply add_le_add_right
+  rw [sub_eq_add_neg]
+  apply le_trans
+  apply aux
+  rw [add_neg_cancel_right, add_neg_cancel_right]
+
 #check (abs_add : ∀ a b : ℝ, |a + b| ≤ |a| + |b|)
 
-example : |a| - |b| ≤ |a - b| :=
-  sorry
+example : |a| - |b| ≤ |a - b| := by
+  have h := abs_add (a - b) b
+  rw [sub_add_cancel] at h
+  linarith
 end
 
 section
@@ -93,7 +109,13 @@ example : x ∣ x ^ 2 := by
    apply dvd_mul_left
 
 example (h : x ∣ w) : x ∣ y * (x * z) + x ^ 2 + w ^ 2 := by
-  sorry
+  apply dvd_add
+  · apply dvd_add
+    · apply dvd_mul_of_dvd_right
+      apply dvd_mul_right
+    apply dvd_mul_left
+  apply dvd_trans h _
+  apply dvd_mul_left
 end
 
 section
@@ -105,7 +127,9 @@ variable (m n : ℕ)
 #check (Nat.lcm_zero_left n : Nat.lcm 0 n = 0)
 
 example : Nat.gcd m n = Nat.gcd n m := by
-  sorry
+  apply Nat.dvd_antisymm
+  repeat'
+    apply Nat.dvd_gcd
+    apply Nat.gcd_dvd_right
+    apply Nat.gcd_dvd_left
 end
-
-

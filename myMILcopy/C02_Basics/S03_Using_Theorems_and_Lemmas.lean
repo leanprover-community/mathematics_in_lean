0ad4e4ea7bc2ec9fa0cc963b21cd3d9737d4a109
mallorydolorfino <mallod2@uw.edu>
2026-06-12 11:30:07 -0700

    doing exercises

--- myMILcopy/C02_Basics/S03_Using_Theorems_and_Lemmas.lean
+++ myMILcopy/C02_Basics/S03_Using_Theorems_and_Lemmas.lean
@@ -44,7 +44,17 @@ example (x : ℝ) : x ≤ x :=
 
 -- Try this.
 example (h₀ : a ≤ b) (h₁ : b < c) (h₂ : c ≤ d) (h₃ : d < e) : a < e := by
-  sorry
+  have h₄ : a < c := by
+    apply lt_of_le_of_lt h₀ h₁
+  have h₅ : a < d := by
+    apply lt_of_lt_of_le h₄ h₂
+  apply lt_trans h₅ h₃
+
+-- again
+example (h₀ : a ≤ b) (h₁ : b < c) (h₂ : c ≤ d) (h₃ : d < e) : a < e := by
+  apply lt_of_le_of_lt h₀
+  apply lt_trans h₁
+  apply lt_of_le_of_lt h₂ h₃
 
 example (h₀ : a ≤ b) (h₁ : b < c) (h₂ : c ≤ d) (h₃ : d < e) : a < e := by
   linarith
@@ -84,23 +94,42 @@ example (h₀ : a ≤ b) (h₁ : c < d) : a + exp c + e < b + exp d + e := by
   apply add_lt_add_of_lt_of_le
   · apply add_lt_add_of_le_of_lt h₀
     apply exp_lt_exp.mpr h₁
-  apply le_refl
+  · apply le_refl
 
-example (h₀ : d ≤ e) : c + exp (a + d) ≤ c + exp (a + e) := by sorry
+example (h₀ : d ≤ e) : c + exp (a + d) ≤ c + exp (a + e) := by
+  apply add_le_add_left
+  rw[exp_le_exp]
+  apply add_le_add_left h₀
 
 example : (0 : ℝ) < 1 := by norm_num
 
 example (h : a ≤ b) : log (1 + exp a) ≤ log (1 + exp b) := by
-  have h₀ : 0 < 1 + exp a := by sorry
+  have h₀ : 0 < 1 + exp a := by
+    apply add_pos
+    · norm_num
+    · apply exp_pos
   apply log_le_log h₀
-  sorry
+  apply add_le_add_left
+  rw[exp_le_exp]
+  exact h
+
+#check sq_nonneg
 
 example : 0 ≤ a ^ 2 := by
-  -- apply?
   exact sq_nonneg a
 
+#check tsub_le_tsub
+
 example (h : a ≤ b) : c - exp b ≤ c - exp a := by
-  sorry
+  apply tsub_le_tsub
+  · norm_num
+  · rw[exp_le_exp]
+    exact h
+
+-- with linarith
+example (h : a ≤ b) : c - exp b ≤ c - exp a := by
+  linarith [exp_le_exp.mpr h]
+
 
 example : 2*a*b ≤ a^2 + b^2 := by
   have h : 0 ≤ a^2 - 2*a*b + b^2
@@ -121,7 +150,27 @@ example : 2*a*b ≤ a^2 + b^2 := by
   linarith
 
 example : |a*b| ≤ (a^2 + b^2)/2 := by
-  sorry
+  rw[abs_le']
+  constructor
+  · have h₁ : 2*a*b ≤ a^2 + b^2 := by
+      have h₃ : 0 ≤ a^2 - 2*a*b + b^2
+      calc
+        a^2 - 2*a*b + b^2 = (a-b)^2 := by ring
+        _ ≥ 0 := by apply pow_two_nonneg
+      linarith
+    linarith
+  · have h₂ : -2*a*b ≤ a^2 + b^2 := by
+      sorry
+    linarith
 
-#check abs_le'.mpr
+example : |a*b| ≤ (a^2 + b^2)/2 := by
+  have h : (0 : ℝ) < 2 := by norm_num
+  apply abs_le'.mpr
+  constructor
+  · rw [le_div_iff₀ h]
+    apply fact1
+  rw [le_div_iff₀ h]
+  apply fact2
 
+
+#check abs_le'
